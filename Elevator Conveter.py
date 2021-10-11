b93c487a9c1e6cac9edebd407225684b03c1cad9
Kroggieboy57 <menonkrishna57@gmail.com>
2021-10-11 20:21:14 +0530

    Elevator code complete

---
+++ Elevator Conveter.py
@@ -0,0 +1,4 @@
+#Ground floor in us is 1st floor but in europe it is 0. This takes in european floor and returns us floor
+eufloor=input("European Floor?")
+usfloor=int(eufloor)+1
+print(f"US floor = {usfloor}")
