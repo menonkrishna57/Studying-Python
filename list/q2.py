fe015705dc73df3d38e11c71473c358413135180
“Kroggieboy57” <menonkrishna57@gmail.com>
2023-03-15 15:30:54 +0530

    Appending values to a tuple

--- list/q2.py
+++ list/q2.py
@@ -1,3 +1,7 @@
 """Write a program to create a tuple of length 5. print the squares of each value."""
+x=()
 for i in range(5):
     a=int(input("Enter Number: "))
+    x+=(a,)
+for i in x:
+    print(i**2)
