c6ebcb50b2f9d06e5b0a6b5f1ad142b8d47aead8
“Kroggieboy57” <menonkrishna57@gmail.com>
2023-03-16 17:49:45 +0530

    Made a function sort the data to likes and dislike

--- Hash code/main.py
+++ Hash code/main.py
@@ -1,4 +1,5 @@
 def inputer():
+    global likes,dislikes
     pc=int(input("Potential Customers: "))
     likes=[]
     dislikes=[]
@@ -8,5 +9,39 @@ def inputer():
         likes.append(lk)
         dislikes.append(dlk)
     print()
-inputer()
 
+def repeater(a):
+    checked=list()
+    final=[]
+    for i in a:
+        if i not in checked:
+            counter=a.count(i)
+            dumper=[counter,i]
+            final.append(dumper)
+            checked.append(i)
+    print(final)
+    
+def likeCalc():
+    global totallikes
+    totallikes=[]
+    for i in likes:
+        for p in i:
+            if p.isnumeric()==False:
+                totallikes.append(p)
+    repeater(totallikes)    
+
+def dislikeCalc():
+    global totaldislikes
+    totaldislikes=[]
+    for i in dislikes:
+        for p in i:
+            if p.isnumeric()==False:
+                totaldislikes.append(p)
+    repeater(totaldislikes)
+
+    
+    
+
+inputer()
+likeCalc()
+dislikeCalc()
